13d998ea8e8cba6d118c2cd19017132f91a5c5d4
Bhatnagar <abhatnagar45@gatech.edu>
2019-02-21 16:39:57 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+	[SerializeField] int speed = 0;
+    	// Start is called before the first frame update
+    	void Start()
+    	{
+        
+    	}
+    	// Update is called once per frame
+    	void Update()
+    	{
+        	float moveHor = Input.GetAxis("Horizontal");
+		float moveVert = Input.GetAxis("Vertical");
+		Vector3 position = transform.position;
+      		position.x += moveHor * speed * Time.deltaTime;
+      		position.z += moveVert * speed * Time.deltaTime;
+      		transform.position = position;	
+    	}
+}
