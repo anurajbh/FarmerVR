9c09e889c48272d73cb3f78b9e95f12296f35559
Bhatnagar <abhatnagar45@gatech.edu>
2019-02-26 12:04:52 -0500

    Terrain changes

---
+++ Assets/Scripts/Grabber.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grabber : MonoBehaviour
+{
+    [SerializeField] GameObject collidingObject;
+    [SerializeField] GameObject objectInHand;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.GetComponent<>())
+        {
+            collidingObject = other.gameObject;
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        collidingObject = null;
+    }	
+}
