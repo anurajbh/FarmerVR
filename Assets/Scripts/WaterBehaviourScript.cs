f9b6e9c565d6bdc52ea63057a457bf6a3a6a1f5b
Bhatnagar <abhatnagar45@gatech.edu>
2019-03-05 17:57:03 -0500

    Water droplet flows on touching the pump, need to get configurable joint to work properly.

--- Assets/Scripts/WaterBehaviourScript.cs
+++ Assets/Scripts/WaterBehaviourScript.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class WaterBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject waterSphere;
+    [SerializeField] Rigidbody waterBody;
+    [SerializeField] MeshRenderer water;
+    Vector3 startPos;
     void Start()
     {
-        
+        water.enabled = false;
+        startPos = waterSphere.transform.position;
     }
 
     // Update is called once per frame
@@ -18,5 +22,20 @@ public class WaterBehaviourScript : MonoBehaviour
     public void OnTriggerEnter(Collider other)
     {
         print("Water is flowing");
+        if(other.tag == "Player")
+        {
+            waterBody.useGravity = true;
+            waterBody.isKinematic = false;
+            Invoke("moveBackToOriginal", 5f);
+            
+        }
     }
+    public void moveBackToOriginal()
+    {
+        water.enabled = true;
+        waterBody.useGravity = false;
+        waterBody.isKinematic = true;
+        waterSphere.transform.position = startPos;
+    }
+
 }
