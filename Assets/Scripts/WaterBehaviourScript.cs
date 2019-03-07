a6d006a5c4101eca7c3921f715e711cbca4c5edf
Bhatnagar <abhatnagar45@gatech.edu>
2019-03-07 18:01:10 -0500

    Demo changes

--- Assets/Scripts/WaterBehaviourScript.cs
+++ Assets/Scripts/WaterBehaviourScript.cs
@@ -5,20 +5,31 @@ using UnityEngine;
 public class WaterBehaviourScript : MonoBehaviour
 {
     [SerializeField] GameObject waterSphere;
+    [SerializeField] GameObject lever;
     [SerializeField] Rigidbody waterBody;
     [SerializeField] MeshRenderer water;
     [SerializeField] float dropTime = 3f;
+    //[SerializeField] float rotateZ = 3f;
+    [SerializeField] float timer = 0f;
+    float rotationThisFrame;
     Vector3 startPos;
+    //float startLeverRotX;
+    //float startLeverRotY;
+    //float startLeverRotZ;
     void Start()
     {
         water.enabled = false;
         startPos = waterSphere.transform.position;
+        /*startLeverRotX = lever.transform.localRotation.x;
+        startLeverRotY = lever.transform.localRotation.y;
+        startLeverRotZ = lever.transform.localRotation.z;*/
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        //rotationThisFrame = rotateZ * Time.deltaTime;
     }
     public void OnTriggerEnter(Collider other)
     {
@@ -27,8 +38,8 @@ public class WaterBehaviourScript : MonoBehaviour
         {
             waterBody.useGravity = true;
             waterBody.isKinematic = false;
+           // lever.transform.Rotate(startLeverRotX, startLeverRotY, rotateZ);
             Invoke("moveBackToOriginal", dropTime);
-            
         }
     }
     public void moveBackToOriginal()
@@ -37,6 +48,7 @@ public class WaterBehaviourScript : MonoBehaviour
         waterBody.useGravity = false;
         waterBody.isKinematic = true;
         waterSphere.transform.position = startPos;
+       // lever.transform.Rotate(startLeverRotX, startLeverRotY, startLeverRotZ);
     }
 
 }
